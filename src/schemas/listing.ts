166c337672e5624093e174ff631938db160bd25f
itsmattsoria <matt.soria@hey.com>
2026-07-29 03:46:53 -0500

    Initial project setup

---
+++ src/schemas/listing.ts
@@ -0,0 +1,15 @@
+import { z } from 'astro/zod';
+
+const listingSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  address: z.string(),
+  latLong: z.string(),
+  website: z.string(),
+  mapUrl: z.string(),
+  type: z.string(),
+  tags: z.array(z.string()),
+  images: z.array(z.string()),
+});
+
+export type Listing = z.infer<typeof listingSchema>;
