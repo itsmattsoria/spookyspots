95794bc4742b2a0bee52c51fbe7024f035619aa4
itsmattsoria <matt.soria@hey.com>
2026-07-29 17:51:39 -0500

    Updated styling and functionality

---
+++ src/schemas/typeColors.ts
@@ -0,0 +1,24 @@
+const typeColors = [
+  {
+    name: 'Coffee Shop',
+    color: '#289B2B',
+  },
+  {
+    name: 'Bar',
+    color: '#F8159D',
+  },
+  {
+    name: 'Restaurant',
+    color: '#731DDC',
+  },
+  {
+    name: 'Store',
+    color: '#F78148',
+  },
+  {
+    name: 'Other',
+    color: '#F78148',
+  },
+];
+
+export { typeColors };
