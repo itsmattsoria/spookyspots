01a270f492aa33839aa778006040c97587faa8df
itsmattsoria <matt.soria@hey.com>
2026-08-17 09:32:25 -0500

    Add carousel to listings, add rating info

--- src/schemas/listing.ts
+++ src/schemas/listing.ts
@@ -14,6 +14,11 @@ const listingSchema = z.object({
   tags: z.string().optional(),
   images: z.array(z.string()).optional(),
   searchTerms: z.string().optional(),
+  rating: z.number().optional(),
+  userRatingCount: z.number().optional(),
+  photo1: z.string().optional(),
+  photo2: z.string().optional(),
+  photo3: z.string().optional(),
   unlisted: z.boolean().default(false),
 });
 
